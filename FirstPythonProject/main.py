105f58557c020406cdd9f9ef44be6f8809271250
Aman-max-ally <amankumarsharma1899@gmail.com>
2026-03-15 13:38:09 +0530

    Add main2.py

--- FirstPythonProject/main.py
+++ FirstPythonProject/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+#I am going to start learning python from scratch
+#first,i'm going to perform some arithmetic
+#Addition
+
+x = 10
+y = 20
+
+grand = x + y
+print("the sum of x and y is",grand)
---
+++ FirstPythonProject/main2.py
@@ -0,0 +1,19 @@
+#I am going to start learning python from scratch
+#first,i'm going to perform some arithmetic
+#Addition
+
+x = 10
+y = 20
+
+grand = x + y
+print("the sum of x and y is :-",grand)
+
+#Now,multiplication
+
+grand1 = x*y
+print("the product of x and y is :-",grand1)
+
+#Now, Division
+
+grand2 = x/y
+print("the division of x and y :-",grand2)
